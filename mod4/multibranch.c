778e3be6ea7435bb5c1d8abb226e2af141fd7e57
Seth Perkins <sethperkins@mail.weber.edu>
2018-01-30 10:15:16 -0700

    new commit

---
+++ mod4/multibranch.c
@@ -0,0 +1,39 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  multibranch.c
+ *
+ *    Description:  nested branches
+ *
+ *        Version:  1.0
+ *        Created:  01/30/2018 10:10:34 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Seth Perkins (), sethperkins@mail.weber.edu
+ *   Organization:  WSU
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+
+// Constants
+
+// Function Prototypes
+
+// Main Function
+int main()
+{
+    int income = 30000
+    if(income)
+    {
+        printf("Income is %d\n", income);
+    }
+    else
+    {
+        printf("no income\n");
+        return 0;
+}
+// Function Definitions
+
+
